958f0380aa6796d8bc86030a4403a2c71357f150
darshansedani22 <105973290+darshansedani22@users.noreply.github.com>
2023-08-07 01:36:10 +0530

    Update model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,9 +1,6 @@
 # Basic Import
 import numpy as np
 import pandas as pd
-import sys
-import os
-sys.path.append(r'D:\Projects\House_Price_Prediction')
 from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
 from src.exception import CustomException
 from src.logger import logging
@@ -12,6 +9,8 @@ from src.utils import save_object
 from src.utils import evaluate_model
 
 from dataclasses import dataclass
+import sys
+import os
 
 @dataclass 
 class ModelTrainerConfig:
@@ -22,7 +21,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array):
+    def initate_model_training(self,train_array,test_array):
         try:
             logging.info('Splitting Dependent and Independent variables from train and test data')
             X_train, y_train, X_test, y_test = (
@@ -62,6 +61,7 @@ class ModelTrainer:
                  obj=best_model
             )
           
+
         except Exception as e:
             logging.info('Exception occured at Model Training')
             raise CustomException(e,sys)
