125cbcdae88cbc44082452f995256835a2cb241d
Darshan Sedani <darshansedani81@gmail.com>
2023-08-07 01:32:22 +0530

    Update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -53,12 +53,3 @@ class DataIngestion:
         except Exception as e:
             logging.info("Exception occured at Data Ingestion Stage")
             raise CustomException(e,sys)
-
-## Run Data Ingestion
-
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiateDataIngesion()
-
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
